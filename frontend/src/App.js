f329a2abfaef5e57e4d2eef3d188fcd72e50e8f5
xcel24 <prateekgoswami1996@gmail.com>
2022-10-09 21:05:47 +0530

    Navbar and Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+
 const App = () => {
   return (
     <>
-    <h1>Welcome To Proshop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
