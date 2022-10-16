eaa88f7317ad7fc47e7dfcf7e3bfcfbfbb00e71b
xcel24 <prateekgoswami1996@gmail.com>
2022-10-16 11:38:22 +0530

    backend setup and basic routing

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -52,7 +52,11 @@ const ProductScreen = ({ match }) => {
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn-block' type='button'>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
                   Add to Cart
                 </Button>
               </ListGroup.Item>
