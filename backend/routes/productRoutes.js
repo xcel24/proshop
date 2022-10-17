83bb5135623f619fd9fe0e59f77712cffb3d17d0
xcel24 <prateekgoswami1996@gmail.com>
2022-10-17 11:53:24 +0530

    getting data from db and backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productsModel.js'
+
+const router = express.Router()
+
+//get all the products
+//GET call
+//Public access
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    res.json(await Product.find({}))
+  })
+)
+
+//get product by id
+//GET call
+//Public access
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({
+        message: 'Product not found',
+      })
+    }
+  })
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -13,14 +14,7 @@ app.get('/', (req, res) => {
   res.send('API running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.send(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 4500
 
