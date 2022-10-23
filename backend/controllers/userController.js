3c0e6175f4d70887e0cc964ce5f7313cb7b9f160
xcel24 <prateekgoswami1996@gmail.com>
2022-10-23 16:59:48 +0530

    added register new user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,4 +44,37 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+//create a new user
+//POST call
+//Public access
+const registerNewUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user) {
+    res.status(400)
+    throw new Error('Bad request, email already exists')
+  } else {
+    const createdUser = await User.create({
+      name,
+      email,
+      password,
+    })
+
+    if (createdUser) {
+      res.status(201).json({
+        _id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        isAdmin: createdUser.isAdmin,
+        token: generateToken(createdUser._id),
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+    }
+  }
+})
+
+export { authUser, getUserProfile, registerNewUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import {
+  authUser,
+  getUserProfile,
+  registerNewUser,
+} from '../controllers/userController.js'
 import protect from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
+router.post('/', registerNewUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
